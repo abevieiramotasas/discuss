f70b2a1dd1c9671672ae65e12511a8c02e21bc71
Abelardo <abevieiramota@gmail.com>
2012-12-25 09:52:33 -0300

    deploy simples

--- discuss/front/views.py
+++ discuss/front/views.py
@@ -4,14 +4,14 @@ from django.template import RequestContext
 from django.conf import settings
 
 def main(request):
-	return render_to_response('index.html', {}, context_instance=RequestContext(request))
+   return render_to_response('index.html', {}, context_instance=RequestContext(request))
 
 def server_side(request):
    code = request.GET.get('code', None)
-	if not code:
-		dialog_url = "https://www.facebook.com/dialog/oauth?client_id=" +
+   if not code:
+      dialog_url = "https://www.facebook.com/dialog/oauth?client_id=" +
 							settings.FACEBOOK_APP_ID + "&redirect_uri=" +
 							settings.FACEBOOK_MY_URL
-		dados = {}
-		dados['dialog_url'] = dialog_url
-		return render_to_response('base.html', dados, context_instance=RequestContext(request))
+      dados = {}
+      dados['dialog_url'] = dialog_url
+      return render_to_response('base.html', dados, context_instance=RequestContext(request))
